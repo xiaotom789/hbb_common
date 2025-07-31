815dc75321f05c9bf8f81545a79e49f4b5bf3af3
xiaotom789 <350912315@qq.com>
2025-07-31 15:42:14 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -68,15 +68,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
-
-let mut map = HashMap::new();
-
-map.insert("password".to_string(), "Xt7010322".to_string());
-
-RwLock::new(map)
-
-};
+    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
 }
 
@@ -357,7 +349,7 @@ pub struct PeerConfig {
 impl Default for PeerConfig {
     fn default() -> Self {
         Self {
-            password: "Xt7010322".to_string(),
+            password: Default::default(),
             size: Default::default(),
             size_ft: Default::default(),
             size_pf: Default::default(),
